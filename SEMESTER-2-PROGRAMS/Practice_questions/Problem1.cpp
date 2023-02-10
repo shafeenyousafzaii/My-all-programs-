1ffc99609bd2041b26d30d877097deb4d6e34e5f
Muhammad Shafeen <112253449+shafeenyousafzaii@users.noreply.github.com>
2023-02-10 19:03:00 +0500

    Semester 2 stuff

---
+++ SEMESTER-2-PROGRAMS/Practice_questions/Problem1.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+#include<vector>
+using namespace std;
--- Semester_1_break/pointer.c
+++ Semester_1_break/pointer.c
@@ -1,4 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
 int main()
 {
     
